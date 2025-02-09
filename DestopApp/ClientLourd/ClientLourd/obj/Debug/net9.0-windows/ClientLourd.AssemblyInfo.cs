6a7152f3224f3f603b00b03efdfc435e8e1ff973
Sflegioner <woteratak33@gmail.com>
2025-02-09 19:00:54 +0100

    Unit Test

--- DestopApp/ClientLourd/ClientLourd/obj/Debug/net9.0-windows/ClientLourd.AssemblyInfo.cs
+++ DestopApp/ClientLourd/ClientLourd/obj/Debug/net9.0-windows/ClientLourd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientLourd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a282dac412884ec013d596a9680790118255f9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e51b68d0c0c97f2e45e3bb82a4faedb26f6de6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientLourd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientLourd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ DestopApp/ClientLourd/TestProject1/APIserviceSite.cs
@@ -0,0 +1,31 @@
+﻿using ClientLourd.MVVM.Model;
+using Xunit;
+namespace TestProject1;
+using ClientLourd.Core; 
+public class APIserviceSite
+{
+    [Fact]
+    public async Task GetSitesAsync()
+    {
+        var result = await ClientLourd.Core.APIserviceSite.GetSitesAsync();
+        Assert.NotNull(result);
+        Assert.IsType<List<SiteModel>>(result);
+    }
+
+    [Fact]
+    public async Task PostSiteAsync()
+    {
+        string siteName = "Test Site";
+        bool result = await ClientLourd.Core.APIserviceSite.PostSiteAsync(siteName);
+        Assert.True(result);
+    }
+
+    [Fact]
+    public async Task DeleteSiteAsync()
+    {
+        string siteName = "Test Site";
+        await ClientLourd.Core.APIserviceSite.PostSiteAsync(siteName); 
+        bool result = await ClientLourd.Core.APIserviceSite.DeleteSiteAsync(siteName);
+        Assert.True(result);
+    }
+}
